7ca6989061836806ce12e3c3b0b2f5f438d1a2d2
Seiichi Inoue <d35inou108@gmail.com>
2019-04-22 22:47:16 +0900

    aaaaaaaaaa

--- abc027/b.py
+++ abc027/b.py
@@ -4,4 +4,12 @@ s = sum(l)
 if s % n != 0:
   print(-1)
   exit()
-print(sum([abs(s/n - i) for i in l]))
+mean, ans = s // n, 0
+for i in range(len(l) - 1):
+    if l[i] < mean:
+        l[i + 1] -= abs(l[i] - mean)
+        ans += 1
+    elif l[i] > mean:
+        l[i + 1] += abs(l[i] - mean)
+        ans += 1
+print(ans)
---
+++ abc028/b.py
@@ -0,0 +1,2 @@
+s = input()
+print(" ".join([str(s.count(t)) for t in ['A', 'B', 'C', 'D', 'E', 'F']]))
