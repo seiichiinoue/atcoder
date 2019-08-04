4818da3bf43f6f848a8f830aa82d180356d5c715
Seiichi Inoue <d35inou108@gmail.com>
2019-08-05 07:20:29 +0900

    yay

--- abc136/d.cpp
+++ abc136/d.cpp
@@ -4,21 +4,21 @@ using namespace std;
 
 int main() {
     char str[max]; cin >> str;
-    int i; char tmp = 'R';
+    int i; char tmp='R';
     int ans[max]={};
     int cnt=0, j=0, len=strlen(str);
-    for (int i=0; i<len;i++) {
+    for (int i=0; i<len; i++) {
         if (tmp==str[i]) {
             cnt++;
             continue;
         }
         if (tmp=='R') {
-            j=i; 
-            ans[j] += cnt/2;
-            ans[j-1] += (cnt+1)/2;
+            j=i;    // 直前のRLのLの場所
+            ans[j] += cnt/2;        //L
+            ans[j-1] += (cnt+1)/2;  //R
         } else {
-            ans[j-1] += cnt/2;
-            ans[j] += (cnt+1)/2;
+            ans[j-1] += cnt/2;      //R
+            ans[j] += (cnt+1)/2;    //L
         }
         cnt = 1;
         tmp=str[i];
