7fa700c85ae33e5c10c20054e0d6e32d24fdbbcf
Seiichi Inoue <d35inou108@gmail.com>
2019-08-21 23:50:41 +0900

    addede new

---
+++ abc138/c.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    vector<double> v(n,0.0); for (int i=0; i<n; ++i) cin >> v[i];
+    sort(v.begin(), v.end());
+    int a, b, c;
+    for (int i=1; i<n; ++i) {
+        v[i] = (v[i]+v[i-1])/2.0;
+        sort(v.begin(), v.end());
+    }
+    cout << fixed << setprecision(12) << v.back() << "\n";
+    return 0;
+}
