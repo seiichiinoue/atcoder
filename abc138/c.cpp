611cc13412b56d92cad05aa15e3e0970a8e33a90
Seiichi Inoue <d35inou108@gmail.com>
2019-08-21 23:58:31 +0900

    fixed

--- abc138/c.cpp
+++ abc138/c.cpp
@@ -5,7 +5,6 @@ int main() {
     int n; cin >> n;
     vector<double> v(n,0.0); for (int i=0; i<n; ++i) cin >> v[i];
     sort(v.begin(), v.end());
-    int a, b, c;
     for (int i=1; i<n; ++i) {
         v[i] = (v[i]+v[i-1])/2.0;
         sort(v.begin(), v.end());
