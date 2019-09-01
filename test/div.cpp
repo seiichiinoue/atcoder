411b4fd90d89a80d680fd0c128c1b12c78a198cc
Seiichi Inoue <d35inou108@gmail.com>
2019-09-01 20:03:19 +0900

    hogee

---
+++ test/div.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a;
+    float b;
+    float c;
+    a = 5 / 2;
+    b = (float)5 / 2;   
+    c = 5.0 / 2.0;      
+    cout << a << " " << b << " " << c << endl;
+}
