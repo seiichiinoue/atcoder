5964e05d12c16394f7d8f7e6b74fc487e7daa788
Seiichi Inoue <d35inou108@gmail.com>
2019-10-27 20:46:01 +0900

    added abc143

---
+++ atcoder/abc143/c.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i=0; i<n; ++i)
+#define rep1(i, n) for (int i=1; i<=n; ++i)
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+
+int main() {
+    int n; cin >> n;
+    string s; cin >> s;
+    char cur = s[0];
+    int ans = 0;
+    rep1(i, n) {
+        if (s[i]==cur) {
+            continue;
+        } else {
+            cur = s[i];
+            ++ans;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
---
+++ atcoder/abc144/c.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i=0; i<n; ++i)
+#define rep1(i, n) for (int i=1; i<=n; ++i)
+#define ALL(v) v.begin(), v.end()
+using namespace std;
+typedef long long ll;
+
+int main() {
+    cout << "hello atcoder!" << endl;
+    return 0;
+}
