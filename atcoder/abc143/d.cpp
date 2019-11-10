7ef470f41076a86e66ca5cf3cefce75a8a019b40
Seiichi Inoue <d35inou108@gmail.com>
2019-11-11 00:34:31 +0900

    done abc143d

--- atcoder/abc143/d.cpp
+++ atcoder/abc143/d.cpp
@@ -2,11 +2,44 @@
 #define rep(i, n) for (int i=0; i<n; ++i)
 #define rep1(i, n) for (int i=1; i<=n; ++i)
 #define ALL(v) v.begin(), v.end()
+#define RALL(v) v.rbegin(), v.rend()
+#define EPS (1e-7)
+#define INF (1e9)
+#define PI (acos(-1))
 using namespace std;
 typedef long long ll;
 
 int main() {
     int n; cin >> n;
-    
+    vector<int> l(n);
+    rep(i, n) cin >> l[i];
+    // ここで昇順になるため，任意のa=l[i]より右にあるものb, cは b+c>aを満たす
+    sort(ALL(l));   // nlogn
+    int ans = 0;
+    // rep(i, n) {
+    //     int a = l[i];
+    //     for (int j=i+1; j<n; ++j) {
+    //         int b = l[j];
+    //         for (int k=j+1; k<n; ++k) {
+    //             int c = l[k];
+    //             if (a+c<=b) {
+    //                 break;
+    //             } else if (a+b<=c) {
+    //                 break;
+    //             } else {
+    //                 ++ans;
+    //             }
+    //         }
+    //     }
+    // }
+    rep(b, n) {
+        rep(a, b) {
+            int ab = l[a]+l[b];
+            int r = lower_bound(ALL(l), ab) - l.begin();
+            int l = b+1;    // c = [l, r)
+            ans += r-l;
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
