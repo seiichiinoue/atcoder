590b91248ef084f2d6bd3b5babaea4e355447351
Seiichi Inoue <d35inou108@gmail.com>
2019-11-20 21:48:03 +0900

    done abc038

---
+++ atcoder/abc038/b.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i=0; i<n; ++i)
+#define rep1(i, n) for (int i=1; i<=n; ++i)
+#define ALL(v) v.begin(), v.end()
+#define RALL(v) v.rbegin(), v.rend()
+#define EPS (1e-7)
+#define INF (1e9)
+#define PI (acos(-1))
+using namespace std;
+typedef long long ll;
+constexpr ll  MOD = (1e9+7);
+constexpr int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+ll factorial(ll n, ll m=2) {
+    // calculate nPm
+    m = max(2LL, m);
+    ll rtn = 1;
+    for (ll i=m; i<=n; i++) {
+        rtn = (rtn * i) % MOD;
+    }
+    return rtn;
+}
+ll modinv(ll a, ll m) {
+    ll b = m, u = 1, v = 0;
+    while (b) {
+        ll t = a / b;
+        a -= t * b;
+        swap(a, b);
+        u -= t * v;
+        swap(u, v);
+    }
+    u %= m;
+    if (u < 0) u += m;
+    return u;
+}
+ll modpow(ll a, ll n) {
+    ll res = 1;
+    while (n > 0) {
+        if (n & 1)
+            res = res * a % MOD;
+        a = a * a % MOD;
+        n >>= 1;
+    }
+    return res;
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    ll h1, w1, h2, w2; 
+    cin >> h1 >> w1 >> h2 >> w2;
+    if (h1 == h2 || h1 == w2 || w1 == w2 || w1 == h2) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+    cout << endl;
+    return 0;
+}
