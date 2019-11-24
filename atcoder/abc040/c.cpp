ae65ae273f7b3b4991e043700709e2258cbd0525
Seiichi Inoue <d35inou108@gmail.com>
2019-11-24 19:02:30 +0900

    done abc040d

--- atcoder/abc040/c.cpp
+++ atcoder/abc040/c.cpp
@@ -61,6 +61,14 @@ ll modpow(ll a, ll n) {
 int main() {
     cin.tie(0);
     ios::sync_with_stdio(0);
-    cout << "hello atcoder!" << endl;
+    ll n; cin >> n;
+    vector<ll> a(n);
+    rep(i, n) cin >> a[i];
+    vector<ll> dp(n, 0);
+    dp[0] = 0; dp[1] = abs(a[1]-a[0]);
+    for (int i=2; i<n; ++i) {
+        dp[i] = min(dp[i-2]+abs(a[i]-a[i-2]), dp[i-1]+abs(a[i]-a[i-1]));
+    }
+    cout << dp[n-1] << endl;
     return 0;
 }
