bba8722da358a4c5d12ded5b979d2f776480bb66
Seiichi Inoue <d35inou108@gmail.com>
2019-12-01 00:46:23 +0900

    done abc054b

---
+++ atcoder/abc054/b.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i=0; i<n; ++i)
+#define rep1(i, n) for (int i=1; i<=n; ++i)
+#define ALL(v) v.begin(), v.end()
+#define RALL(v) v.rbegin(), v.rend()
+#define EPS (1e-7)
+#define INF (1e9)
+#define PI (acos(-1))
+using namespace std;
+typedef long long ll;
+constexpr ll  MOD = (1e9+7);
+constexpr int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+constexpr int lcm(int a, int b) { return a / gcd(a, b) * b; }
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+ll factorial(ll n, ll m=2) {
+    // calculate n!
+    m = max(2LL, m);
+    ll rtn = 1;
+    for (ll i=m; i<=n; i++) {
+        rtn = (rtn * i) % MOD;
+    }
+    return rtn;
+}
+ll modinv(ll a, ll m) {
+    ll b = m, u = 1, v = 0;
+    while (b) {
+        ll t = a / b;
+        a -= t * b;
+        swap(a, b);
+        u -= t * v;
+        swap(u, v);
+    }
+    u %= m;
+    if (u < 0) u += m;
+    return u;
+}
+ll modpow(ll a, ll n) {
+    ll res = 1;
+    while (n > 0) {
+        if (n & 1)
+            res = res * a % MOD;
+        a = a * a % MOD;
+        n >>= 1;
+    }
+    return res;
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    int n, m; cin >> n >> m;
+    vector<string> a(n), b(m);
+    rep(i, n) cin >> a[i];
+    rep(i, m) cin >> b[i];
+    bool exist = false;
+    rep(i, n) {
+        rep(j, n) {
+            if (i+m-1>=n || j+m-1>=n) continue;
+            bool match = true;
+            rep(y, m) {
+                rep(x, m) {
+                    if(a[i+y][j+x] != b[y][x]) match = false;
+                }
+            }
+            if (match) exist = true;
+        }
+    }
+    if (exist) puts("Yes");
+    else puts("No");
+    return 0;
+}
