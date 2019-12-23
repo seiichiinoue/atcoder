d2d11e063c2243ff07c49332cc5d352df9e83825
Seiichi Inoue <d35inou108@gmail.com>
2019-12-23 15:11:53 +0900

    done abc148f

--- atcoder/abc148/f.cpp
+++ atcoder/abc148/f.cpp
@@ -13,20 +13,20 @@ constexpr ll  MOD = (1e9+7);
 constexpr int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
 constexpr int lcm(int a, int b) { return a / gcd(a, b) * b; }
 
-// template<class T> inline bool chmin(T& a, T b) {
-//     if (a > b) {
-//         a = b;
-//         return true;
-//     }
-//     return false;
-// }
-// template<class T> inline bool chmax(T& a, T b) {
-//     if (a < b) {
-//         a = b;
-//         return true;
-//     }
-//     return false;
-// }
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
 // ll factorial(ll n, ll m=2) {
 //     // calculate n!
 //     m = max(2LL, m);
@@ -120,32 +120,45 @@ int main() {
     ios::sync_with_stdio(false);
     int n, u, v; cin >> n >> u >> v;
     u--; v--;
-    // vector<vector<int>> to(n-1);
-    vector<vector<int>> dp(n, vector<int>(n, INF));
-    map<int, int> mp;
+    vector<vector<int>> to(n);
     rep(i, n-1) {
         int a, b; cin >> a >> b;
         a--; b--;
-        // to[a].push_back(b);
-        // to[b].push_back(a);
-        dp[a][b] = 1;
-        dp[b][a] = 1;
-        mp[a]++;
-        mp[b]++;
+        to[a].push_back(b);
+        to[b].push_back(a);
     }
-    rep(k,n) {
-        rep(i,n) {
-            rep(j,n){
-                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j]);
-            }
+    vector<ll> u_dist(n, INF), v_dist(n, INF);
+    vector<bool> visited(n, false);
+    queue<int> q;
+    q.push(u);
+    u_dist[u] = 0;
+    visited[u] = true;
+    while (!q.empty()) {
+        auto x = q.front(); q.pop();
+        rep(i, to[x].size()) {
+            if (visited[to[x][i]]) continue;
+            visited[to[x][i]] = true;
+            u_dist[to[x][i]] = u_dist[x]+1;
+            q.push(to[x][i]);
         }
     }
-    cout << dp[u][v]/2 << endl;
-    rep(i, n) {
-        rep(j, n) {
-            if (i != j && dp[i][j] != INF)
-            cout << "cost from " << i << " to " << j << ": " << dp[i][j] << endl;
+    rep(i, n) visited[i] = false;
+    queue<int> q2;
+    q2.push(v);
+    v_dist[v] = 0;
+    visited[v] = true;
+    while (!q2.empty()) {
+        auto x = q2.front(); q2.pop();
+        rep(i, to[x].size()) {
+            if (visited[to[x][i]]) continue;
+            visited[to[x][i]] = true;
+            v_dist[to[x][i]] = v_dist[x]+1;
+            q2.push(to[x][i]);
         }
     }
+    ll ans = 0;
+    rep(i, n) if (v_dist[i] > u_dist[i]) chmax(ans, v_dist[i]);
+    // rep(i, n) cout << v_dist[i] << " " << u_dist[i] << endl;
+    cout << ans-1 << endl;
     return 0;
 }
