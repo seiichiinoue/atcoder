d148dfb60676e831ba287f3cdc23298cb4334c95
Seiichi Inoue <d35inou108@gmail.com>
2020-03-28 04:58:51 +0900

    done new contests

---
+++ atcoder/abc159/b.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i=0; i<n; ++i)
+#define rep1(i, n) for (int i=1; i<=n; ++i)
+#define ALL(v) v.begin(), v.end()
+#define RALL(v) v.rbegin(), v.rend()
+#define EPS (1e-7)
+#define INF (1e9)
+#define PI (acos(-1))
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+constexpr ll  MOD = (1e9+7);
+constexpr int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+constexpr int lcm(int a, int b) { return a / gcd(a, b) * b; }
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+ll factorial(ll n, ll m=2) {
+    // calculate n!
+    m = max(2LL, m);
+    ll rtn = 1;
+    for (ll i=m; i<=n; i++) {
+        rtn = (rtn * i) % MOD;
+    }
+    return rtn;
+}
+ll modinv(ll a, ll m) {
+    ll b = m, u = 1, v = 0;
+    while (b) {
+        ll t = a / b;
+        a -= t * b;
+        swap(a, b);
+        u -= t * v;
+        swap(u, v);
+    }
+    u %= m;
+    if (u < 0) u += m;
+    return u;
+}
+ll modpow(ll a, ll n) {
+    ll res = 1;
+    while (n > 0) {
+        if (n & 1)
+            res = res * a % MOD;
+        a = a * a % MOD;
+        n >>= 1;
+    }
+    return res;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    string s; cin >> s;
+    string rs; rs = s; reverse(ALL(rs));
+    if (!(s == rs)) { puts("No"); return 0; }
+    int i = 0, j = (s.size()-1)/2-1;
+    while (i<j) {
+        if (!(s[i++] == s[j--])) {
+            puts("No");
+            return 0;
+        }
+    }
+    int k = (s.size()+3)/2-1, l=s.size()-1;
+    while (k<l) {
+        if (!(s[k++] == s[l--])) {
+            puts("No");
+            return 0;
+        }
+    }
+    puts("Yes");
+    return 0;
+}
