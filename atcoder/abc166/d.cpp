4f953e35b49a8c39c23e22d7cd8fd09a78e25451
Seiichi Inoue <d35inou108@gmail.com>
2020-05-05 18:46:08 +0900

    added abc166

---
+++ atcoder/abc166/d.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i=0; i<n; ++i)
+#define rep1(i, n) for (int i=1; i<=n; ++i)
+#define ALL(v) v.begin(), v.end()
+#define RALL(v) v.rbegin(), v.rend()
+#define EPS (1e-7)
+#define INF (1e9)
+#define PI (acos(-1))
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+constexpr ll  MOD = (1e9+7);
+constexpr int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+constexpr int lcm(int a, int b) { return a / gcd(a, b) * b; }
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+ll factorial(ll n, ll m=2) {
+    // calculate n!
+    m = max(2LL, m);
+    ll rtn = 1;
+    for (ll i=m; i<=n; i++) {
+        rtn = (rtn * i) % MOD;
+    }
+    return rtn;
+}
+ll modinv(ll a, ll m) {
+    ll b = m, u = 1, v = 0;
+    while (b) {
+        ll t = a / b;
+        a -= t * b;
+        swap(a, b);
+        u -= t * v;
+        swap(u, v);
+    }
+    u %= m;
+    if (u < 0) u += m;
+    return u;
+}
+ll modpow(ll a, ll n) {
+    ll res = 1;
+    while (n > 0) {
+        if (n & 1)
+            res = res * a % MOD;
+        a = a * a % MOD;
+        n >>= 1;
+    }
+    return res;
+}
+
+int main() {
+    ll x; cin >> x;
+    const ll n = 1000;
+    vector<ll> convert(2 * n + 1);
+    for (ll num = -n; num <= n; num++) {
+        convert[n + num] = num * num * num * num * num;
+    }
+    for (ll i = -n; i <= n; i++) {
+        for (ll j = -n; j <= n; j++) {
+            if (convert[n + i] - convert[n + j] == x) {
+                cout << i << " " << j << endl;
+                return 0;
+            }
+        }
+    }
+}
