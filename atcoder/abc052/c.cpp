34e051a9e20d7f13d0e549d7cfbd666f6432e6c2
Seiichi Inoue <d35inou108@gmail.com>
2020-09-13 17:00:10 +0900

    updated

---
+++ atcoder/abc052/c.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i=0; i<n; ++i)
+#define rep1(i, n) for (int i=1; i<=n; ++i)
+#define ALL(v) v.begin(), v.end()
+#define RALL(v) v.rbegin(), v.rend()
+#define EPS (1e-7)
+#define INF (1e9)
+#define PI (acos(-1))
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> P;
+constexpr ll  MOD = (1e9+7);
+constexpr int gcd(int a, int b) { return b ? gcd(b, a % b) : a; }
+constexpr int lcm(int a, int b) { return a / gcd(a, b) * b; }
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+ll factorial(ll n, ll m=2) {
+    // calculate n!
+    m = max(2LL, m);
+    ll rtn = 1;
+    for (ll i=m; i<=n; i++) {
+        rtn = (rtn * i) % MOD;
+    }
+    return rtn;
+}
+ll modinv(ll a, ll m) {
+    ll b = m, u = 1, v = 0;
+    while (b) {
+        ll t = a / b;
+        a -= t * b;
+        swap(a, b);
+        u -= t * v;
+        swap(u, v);
+    }
+    u %= m;
+    if (u < 0) u += m;
+    return u;
+}
+ll modpow(ll a, ll n) {
+    ll res = 1;
+    while (n > 0) {
+        if (n & 1)
+            res = res * a % MOD;
+        a = a * a % MOD;
+        n >>= 1;
+    }
+    return res;
+}
+
+vector<P> factorizer(ll n) {
+    vector<P> facts;
+    for (ll p=2; p*p<=n; ++p) {
+        if (n % p != 0) continue;
+        int num = 0;
+        while (n % p == 0) { ++num; n /= p; }
+        facts.push_back(P(p, num));
+    }
+    if (n != 1) facts.push_back(P(n, 1));
+    return facts;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    int n; cin >> n;
+    vector<ll> v(n+1, 0);
+    for (int i=2; i<=n; ++i) {
+        auto res = factorizer(i);
+        for (auto prime_exp : res) {
+            v[prime_exp.first] += prime_exp.second;
+        }
+    }
+    ll ans = 1;
+    for (int i=2; i<=n; ++i) {
+        if (v[i] == 0) continue;
+        ans *= v[i] + 1;
+        ans %= MOD;
+    }
+    cout << ans << endl;
+    return 0;
+}
